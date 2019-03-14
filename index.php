a2e1edcc6d76546d11924a07dab7c0e6e3640887
naftaleno <analistadesarrollo@emmanuelips.co>
2019-03-14 08:19:26 -0500

    prueba 1

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>prueba  uno</title>
 </head>
+<header>
+<h3>BIENVENIDO A EMMANUEL</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+	 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+		nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+		pariatur. Excepteur sint occaecat cupidatat non proident, 
+	sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</header>
 <body>
 
 <?php
@@ -20,8 +30,3 @@ echo "Bienvenido ";
 
 </body>
 </html>
-
-
-
-
-
