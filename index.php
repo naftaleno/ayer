4309a734764f30577d9483f172f4ac880447af46
naftaleno <analistadesarrollo@emmanuelips.co>
2019-03-14 08:24:15 -0500

    botones

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>prueba  uno</title>
 </head>
+<link href="dise.html" type="text/css" rel="stylesheet" >
 <header>
 <h3>BIENVENIDO A EMMANUEL</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
@@ -10,22 +11,12 @@
 	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
 		nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
 		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-		pariatur. Excepteur sint occaecat cupidatat non proident, 
+		pariatur. Excepteur sint occaecat cupidatat non proident,
 	sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </header>
 <body>
-
-<?php
-
-echo "Bienvenido ";
-
-
-
-
-
-
-
-?>
+	<button>inicio</button>
+<button>informacion</button>
 
 
 </body>
